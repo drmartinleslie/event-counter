fa118779157fa459e22eda87056c9b5a9e55479d
Martin Leslie <mrmartinleslie@gmail.com>
2018-05-19 17:57:45 -0700

    rethink actor pruning strategy

--- src/main/scala/cloud/leslie/eventcounter/EventCounterActor.scala
+++ src/main/scala/cloud/leslie/eventcounter/EventCounterActor.scala
@@ -2,16 +2,19 @@ package cloud.leslie.eventcounter
 
 import akka.actor.{Actor, Props, Timers}
 
-import scala.concurrent.duration.{Duration, _}
+import scala.concurrent.duration._
 
-class EventCounterActor(initialDataLifespan: Duration) extends Actor with Timers {
+class EventCounterActor(initialDataLifespan: FiniteDuration) extends Actor with Timers {
   import EventCounterActor._
   val eventCounter = new EventCounter(initialDataLifespan)
-  timers.startSingleTimer("prune", Prune, 1.minute)
+  val TimerKey = "Prune"
 
   def receive = {
     case AddEvent =>
       eventCounter.addEvent()
+      if (!timers.isTimerActive(TimerKey)) {
+        timers.startSingleTimer(TimerKey, Prune, eventCounter.dataLifespan)
+      }
 
     case SetDataLifespan(newLifespan) =>
       eventCounter.dataLifespan = newLifespan
@@ -24,18 +27,21 @@ class EventCounterActor(initialDataLifespan: Duration) extends Actor with Timers
 
     case Prune =>
       eventCounter.prune()
+      if (!eventCounter.isEmpty && !timers.isTimerActive(TimerKey)) {
+        timers.startSingleTimer(TimerKey, Prune, eventCounter.dataLifespan)
+      }
   }
 
 }
 
 object EventCounterActor {
-  def props(dataLifespan: Duration = 5.minutes): Props = Props(classOf[EventCounterActor], dataLifespan)
+  def props(dataLifespan: FiniteDuration = 5.minutes): Props = Props(classOf[EventCounterActor], dataLifespan)
 
   case object AddEvent
-  case class GetNumberEvents(duration: Duration)
+  case class GetNumberEvents(duration: FiniteDuration)
   case class NumberEvents(value: Int)
-  case class SetDataLifespan(newLifeSpan: Duration)
+  case class SetDataLifespan(newLifeSpan: FiniteDuration)
   case object GetDataLifespan
-  case class DataLifespan(value: Duration)
+  case class DataLifespan(value: FiniteDuration)
   case object Prune
 }
